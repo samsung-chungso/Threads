65f1e8c1d44a8cc4b23d193439060983daa791cb
yongwonJ <yongwonjang1995@gmail.com>
2024-09-13 09:31:03 +0900

    add gyro monitoring in python

--- balancing_first.ino
+++ balancing_first.ino
@@ -1,35 +1,17 @@
 #include "Wire.h"
 #include <MPU6050_light.h>
 
-//칼만 계수 및 자이로
+MPU6050 mpu(Wire);
+unsigned long timer = 0;
+
 float kalman_y = 0.0;  // Estimated state (yaw)
 float P_y = 1.0;       // Error covariance
 float Q_y = 0.001;     // Process noise
 float R_y = 0.03;      // Measurement noise
 
-MPU6050 mpu(Wire);
-unsigned long timer = 0;
-unsigned long previousTime = 0;
-
-//pid 요소
-float Kp = 2.0;  // Proportional gain
+float Kp = 1.0;  // Proportional gain
 float Ki = 0.0;  // Integral gain
-float Kd = 1.0;  // Derivative gain
-
-float setpoint = 9.0; // Desired yaw angle (0 degrees)
-float integral = 0.0;
-float previousError = 0.0;
-
-// Motor control pins
-const int motor1_Pin1 = 9;
-const int motor1_Pin2 = 10;
-const int motor2_Pin1 = 11;
-const int motor2_Pin2 = 12;
-
-const long interval =30;
-unsigned long lastTime;
-double elapsedTime;
-
+float Kd = 0.0;  // Derivative gain
 
 void setup() {
   Serial.begin(9600);
@@ -38,58 +20,51 @@ void setup() {
   byte status = mpu.begin();
   Serial.print(F("MPU6050 status: "));
   Serial.println(status);
-  while(status!=0){ } // stop everything if could not connect to MPU6050
+  while(status != 0) { } // stop everything if could not connect to MPU6050
   
   Serial.println(F("Calculating offsets, do not move MPU6050"));
   delay(1000);
-  // mpu.upsideDownMounting = true; // uncomment this line if the MPU6050 is mounted upside-down
   mpu.calcOffsets(); // gyro and accelero
   Serial.println("Done!\n");
-
-  pinMode(motor1_Pin1, OUTPUT);
-  pinMode(motor1_Pin2, OUTPUT);
-  pinMode(motor2_Pin1, OUTPUT);
-  pinMode(motor2_Pin2, OUTPUT);
-
 }
 
 void loop() {
-  unsigned long currentTime = millis();
-  if((currentTime - previousTime) >= interval){ 
-    mpu.update();
-    float yaw = getFilteredYaw(mpu.getAngleY());
-    Serial.println(yaw);
-    float error = setpoint - yaw;  // Error between desired and current yaw
-    elapsedTime = (double)(currentTime - lastTime) / 1000;
-    integral += error * elapsedTime; // Integral term using 100ms interval
+  mpu.update();
+  
+  if((millis() - timer) > 10) { // print data every 10ms
+    float x = mpu.getAngleY();
+    float x_fil = getFilteredYaw(x);
     
-    float derivative = (error - previousError) / elapsedTime;  // Derivative term
-    float correction = Kp * error + Ki * integral + Kd * derivative;  // PID output
-    int motorOutput = (int)constrain(correction, -255, 255);
-    setMotorPWM(motorOutput);
-	  previousTime = currentTime;
-  }
+    // Yaw 값을 시리얼로 전송
+    sendYaw(x_fil);  // 필터링된 Yaw 값을 전송
+    
+    // PID 값 업데이트 (시리얼 수신)
+    if (Serial.available()) {
+      String receivedData = Serial.readStringUntil('\n');  // \n 까지 데이터를 읽음
+      sscanf(receivedData.c_str(), "%f %f %f", &Kp, &Ki, &Kd);  // P, I, D 값 파싱
+      Serial.print("New PID values: ");
+      Serial.print(Kp); Serial.print(", ");
+      Serial.print(Ki); Serial.print(", ");
+      Serial.println(Kd);
+    }
 
+    timer = millis();
+  }
 }
 
 float getFilteredYaw(float raw_yaw) {
   // Kalman filter update
-  P_y += Q_y;                              // Prediction update
-  float K_y = P_y / (P_y + R_y);           // Compute Kalman gain
+  P_y += Q_y;  // Prediction update
+  float K_y = P_y / (P_y + R_y);  // Compute Kalman gain
   kalman_y = kalman_y + K_y * (raw_yaw - kalman_y);  // Update estimate
-  P_y = (1 - K_y) * P_y;                   // Update error covariance
+  P_y = (1 - K_y) * P_y;  // Update error covariance
   return kalman_y;
 }
-void setMotorPWM(int pwm) {
-  if (pwm > 0) {
-    analogWrite(motor1_Pin1, pwm);
-    digitalWrite(motor1_Pin2, LOW);
-    analogWrite(motor2_Pin1, pwm);
-    digitalWrite(motor2_Pin2, LOW);
-  } else {
-    analogWrite(motor1_Pin2, -pwm);
-    digitalWrite(motor1_Pin1, LOW);
-    analogWrite(motor2_Pin2, -pwm);
-    digitalWrite(motor2_Pin1, LOW);
-  }
+
+void sendYaw(float yaw) {
+  byte* byteYaw = (byte*)(void*)&yaw;  // float을 바이트 배열로 변환
+
+  Serial.write(0xf5);  // Start byte
+  Serial.write(0x04);  // Length byte (float는 4바이트)
+  Serial.write(byteYaw, 4);  // float의 4바이트 전송
 }
